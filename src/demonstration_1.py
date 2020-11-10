43558df084a58c46ce8dcdaa35d1875c26127aff
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-11-09 19:46:17 -0500

    finished lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -35,22 +35,85 @@ class Stack:
             return None
         return self.items[-1]
 
+# UNDERSTAND
+# to move through a stack, we hanve to remove items
+# we would need to store them somewhere else so we don't lose them
+# we should not directly use a linked list or an array
+# we should build off of the Stack class
+
 class MaxStack:
     def __init__(self):
         # Your code here
+        self.stack = Stack()
+        # more stuff here?
 
 
     def push(self, item):
         """Add a new item onto the top of our stack."""
         # Your code here
+        self.stack.push(item)
+        # update max as necessary
+        if self.max < item:
+            self.max = item
 
 
     def pop(self):
         """Remove and return the top item from our stack."""
         # Your code here
+        item = self.stack.pop()
+        # check if we're removing the max
+        if item == max:
+            # if so, we need to update self.max
+            new_max = self.find_max()
+            self.max = new_max
+        return item
 
 
     def get_max(self):
         """The last item in maxes_stack is the max item in our stack."""
         # Your code here
+        # we can't go inside the stack so...
+        # Approach 1: pop everything off, find the max, and push it back on
+        # trick: when we push it back on, we want everything to stay in the same order
+        # we can't change the order of the stack in any way
+        # values = []
+        # element = self.stack.pop()
+        # cur_max = None
+        # while element is not None:
+            # values.append(element)
+        #     if cur_max is None or cur_max < element:
+        #         cur_max = element
+        #     element = self.stack.pop()
 
+        # # loop:
+        # for i in range(len(values) -1, -1, -1):
+        #     # start with i = len(values) - 1, go up to (but not including) i = -1, decrement i by -1 on each loop
+        #     # JS translation: for (int i = len(values) - 1; i > -1; i--)
+        #     # we're going backwards through the range
+        #     element = values[i]
+        #     self.push(element)
+
+        
+        # # stack: 1, 2, 3
+        # # values = [3, 2, 1], stack = []
+        # return cur_max
+
+        # This works but it's pretty slow
+
+        # Approach 2: we change push so that it keeps track of max
+
+# What we want the code to do
+# we make a stack with values
+max_stack = MaxStack()
+max_stack.push(1)
+max_stack.push(2)
+max_stack.push(3)
+# this should give us the maximum value (which should be at the top)
+max = max_stack.get_max()
+# the print statement should be true
+print(max == 3)
+
+# if we remove the top value, the next one should be the max
+max_stack.pop()
+# the print statement should be true
+print(max_stack.get_max() == 2)
